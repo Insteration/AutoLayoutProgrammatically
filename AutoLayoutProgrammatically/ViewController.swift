17c7cdea5c50daa3459076891c994e681152b5b7
Артём <noisa@live.ru>
2018-12-20 17:14:41 +0200

    added many functions with auto layout

--- AutoLayoutProgrammatically/ViewController.swift
+++ AutoLayoutProgrammatically/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_brown"))
         // this enable autolayout for our image view
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
@@ -25,7 +26,7 @@ class ViewController: UIViewController {
         textView.text = "Join to our army of brown bears!"
         textView.font = UIFont.boldSystemFont(ofSize: 24)
         textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.backgroundColor = .yellow
+        textView.backgroundColor = .white
         textView.textAlignment = .center
         textView.isEditable = false
         textView.isScrollEnabled = false
@@ -39,9 +40,9 @@ class ViewController: UIViewController {
         
         //        let imageView = UIImageView(image: #imageLiteral(resourceName: "bear_brown"))
         
-        view.addSubview(bearImageView)
+//        view.addSubview(bearImageView)
         view.addSubview(descriptionTextView)
-        setupView()
+//        setupView()
         setupLayout()
         
         //        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
@@ -56,20 +57,43 @@ class ViewController: UIViewController {
         
     }
     private func setupLayout() {
+        // added blue
+        let topImageContainerView = UIView()
+//        topImageContainerView.backgroundColor = .yellow
+        view.addSubview(topImageContainerView)
+//        topImageContainerView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        // enable auto layout
+        topImageContainerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        topImageContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        
+        topImageContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        
+        topImageContainerView.addSubview(bearImageView)
+        bearImageView.centerXAnchor.constraint(equalTo: topImageContainerView.centerXAnchor).isActive = true
+        bearImageView.centerYAnchor.constraint(equalTo: topImageContainerView.centerYAnchor).isActive = true
+        bearImageView.heightAnchor.constraint(equalTo: topImageContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
+//        topImageContainerView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        topImageContainerView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        topImageContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
+        
+        
         // setup layout image bear
-        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+//        bearImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//        bearImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+//        bearImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+//        bearImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
         
         // setup layout description text
-        descriptionTextView.topAnchor.constraint(equalTo: bearImageView.bottomAnchor, constant: 120).isActive = true
+        descriptionTextView.topAnchor.constraint(equalTo: topImageContainerView.bottomAnchor).isActive = true
         descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
-    private func setupView() {
-        view.backgroundColor = .yellow
-    }
+//    private func setupView() {
+//        view.backgroundColor = .white
+//    }
 }
 
